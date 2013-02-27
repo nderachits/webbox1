b16f8defdf131bb7c08580997317d78153cd0fa5
Mikalai Dzerachyts <n.derachits@gmail.com>
2013-02-27 22:11:56 +0200

    arraytest added

---
+++ src-test/arraytest.js
@@ -0,0 +1,12 @@
+/**
+ * User: nike
+ * Date: 2/27/13
+ */
+
+TestCase("ArrayTest", {
+   "test array splice should modify arr" : function () {
+       var arr = [1,2,3,4,5];
+       var res = arr.splice(2,3);
+       assertEquals([1,2], arr);
+   }
+});
