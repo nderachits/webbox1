5891b70131c3f2a02f9cf162f2774cb9c2d46363
Mikalai Dzerachyts <n.derachits@gmail.com>
2013-03-23 21:58:22 +0200

    alert mocking

--- src-test/codecademy-test.js
+++ src-test/codecademy-test.js
@@ -6,5 +6,21 @@
 TestCase("BasicJavaScriptTest", {
     "testBasic" : function() {
         assertEquals("should be equal", 5, "Hello".length);
+    },
+    "testAlertConfirmPromt" : function() {
+        window.confirm = function() {
+            // This will get executed instead of showing a browser prompt message
+            return true;
+        };
+        window.alert = function() {
+        };
+
+        window.prompt = function() {
+            return "val1";
+        };
+
+        assertTrue(confirm("Hi!"));
+        alert();
+        assertEquals("val1",prompt("Enter something"));
     }
 });
